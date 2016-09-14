45fa0c1e27f76804a9e6fa5f21c9edc4d1a8d85f
Hernan Zalazar <hernan.zalazar@gmail.com>
2016-09-14 12:08:23 -0300

    Make classes closed by default

--- JWTDecode/A0JWT.swift
+++ JWTDecode/A0JWT.swift
@@ -24,7 +24,7 @@ import Foundation
 
 /// Decodes a JWT
 @objc(A0JWT)
-open class _JWT: NSObject {
+public class _JWT: NSObject {
 
     var jwt: JWT
 
@@ -33,27 +33,27 @@ open class _JWT: NSObject {
     }
 
     /// token header part
-    open var header: [String: Any] {
+    public var header: [String: Any] {
         return self.jwt.header
     }
 
     /// token body part or claims
-    open var body: [String: Any] {
+    public var body: [String: Any] {
         return self.jwt.body
     }
 
     /// token signature part
-    open var signature: String? {
+    public var signature: String? {
         return self.jwt.signature
     }
 
     /// value of the `exp` claim
-    open var expiresAt: Date? {
+    public var expiresAt: Date? {
         return self.jwt.expiresAt as Date?
     }
 
     /// value of the `expired` field
-    open var expired: Bool {
+    public var expired: Bool {
         return self.jwt.expired
     }
 
@@ -64,7 +64,7 @@ open class _JWT: NSObject {
 
     :returns: a new instance of `A0JWT` that holds the decode token
     */
-    open class func decode(jwt jwtValue: String) throws -> _JWT {
+    public class func decode(jwt jwtValue: String) throws -> _JWT {
         let jwt = try DecodedJWT(jwt: jwtValue)
         return _JWT(jwt: jwt)
     }
