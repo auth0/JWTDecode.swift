3f2a19d4f9e93f84ba61d293aae3e6d410c12921
Rita Zerrizuela <zeta@widcket.com>
2020-10-13 20:16:07 -0300

    Use upToNextMajor for SPM dependencies

--- Package.swift
+++ Package.swift
@@ -6,25 +6,21 @@ import PackageDescription
 let package = Package(
     name: "JWTDecode",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "JWTDecode",
-            targets: ["JWTDecode"]),
+            targets: ["JWTDecode"])
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-         .package(url: "https://github.com/Quick/Quick", from: "3.0.0"),
-         .package(url: "https://github.com/Quick/Nimble", from: "9.0.0")
+         .package(url: "https://github.com/Quick/Quick", .upToNextMajor(from: "3.0.0")),
+         .package(url: "https://github.com/Quick/Nimble", .upToNextMajor(from: "9.0.0"))
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "JWTDecode",
             dependencies: [],
             path: "JWTDecode"),
         .testTarget(
             name: "JWTDecode.swiftTests",
-            dependencies: ["JWTDecode", "Quick", "Nimble"]),
+            dependencies: ["JWTDecode", "Quick", "Nimble"])
     ]
 )
