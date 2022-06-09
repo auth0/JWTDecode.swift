c42dcac4df6f8aa96da1f7d14367b98ff18e1303
Rita Zerrizuela <zeta@widcket.com>
2022-06-08 23:46:43 -0300

    Update playground

--- JWTDecode.playground/Contents.swift
+++ JWTDecode.playground/Contents.swift
@@ -1,57 +1,65 @@
-/*: 
-# JWTDecode.swift
-A swift framework to help you decode a [JWT](http://jwt.io) token in your iOS/OSX applications
-*/
-
-//: First we need to import JWTDecode framework
+/*:
+ # JWTDecode.swift
+ Easily decode a [JWT](https://jwt.io/) and access the claims it contains.
+ */
+/*:
+ ## Getting Started
+ First, import the JWTDecode framework.
+ */
 import JWTDecode
-
-/*: 
-Then paste here the token you wish to decode
-*/
-let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL3NhbXBsZXMuYXV0aDAuY29tIiwic3ViIjoiYXV0aDB8MTAxMDEwMTAxMCIsImF1ZCI6Imh0dHBzOi8vc2FtcGxlcy5hdXRoMC5jb20iLCJleHAiOjEzNzI2NzQzMzYsImlhdCI6MTM3MjYzODMzNiwianRpIjoicXdlcnR5MTIzNDU2IiwibmJmIjoxMzcyNjM4MzM2LCJlbWFpbCI6InVzZXJAaG9zdC5jb20iLCJjdXN0b20iOlsxLDIsM119.JeMRyHLkcoiqGxd958B6PABKNvhOhIgw-kbjecmhR_E"
-//: You can generate a new token in [jwt.io](http://jwt.io)
-
-/*: 
-## Decode
-Then here we try to decode it calling `decode(token: String) -> JWT` that will return an object with all the decoded values
-*/
+/*:
+ Then, paste here the token you wish to decode.
+ */
+let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL3NhbXBsZXMuYXV0aDAuY29tIiwic3ViIjoiYXV0aDB8MTAxMDEwMTAxMCIsImF1ZCI6Imh0dHBzOi8vc2FtcGxlcy5hdXRoMC5jb20iLCJleHAiOjEzNzI2NzQzMzYsImlhdCI6MTM3MjYzODMzNiwianRpIjoicXdlcnR5MTIzNDU2IiwibmJmIjoxMzcyNjM4MzM2LCJlbWFpbCI6InVzZXJAZXhhbXBsZS5jb20iLCJjdXN0b20iOlsxLDIsM119.i71AXss9VcKxiuULNMq7LqAXjtjweNlXPVNf529vXdM"
+//: You can generate a new token in [jwt.io](http://jwt.io).
+/*:
+ ## Decode
+ Try to decode it by calling `decode(token:)`. That will return an object with all the decoded values.
+ */
 do {
     let jwt = try decode(jwt: token)
 
-//: ### JWT parts
+    // JWT parts
 
-//: Header dictionary
+    // Header dictionary
     jwt.header
-//: Claims in token body
+
+    // Claims in token body
     jwt.body
-//: Token signature
+
+    // Token signature
     jwt.signature
 
-//: ### Registered Claims
+    // Registered Claims
 
-//: "aud" (Audience)
+    // "aud" (Audience)
     jwt.audience
-//: "sub" (Subject)
+
+    // "sub" (Subject)
     jwt.subject
-//: "jti" (JWT ID)
+
+    // "jti" (JWT ID)
     jwt.identifier
-//: "iss" (Issuer)
+
+    // "iss" (Issuer)
     jwt.issuer
-//: "nbf" (Not Before)
+
+    // "nbf" (Not Before)
     jwt.notBefore
-//: "iat" (Issued At)
+
+    // "iat" (Issued At)
     jwt.issuedAt
-//: "exp" (Expiration Time)
+
+    // "exp" (Expiration Time)
     jwt.expiresAt
-//: ### Custom Claims
-//: If we also have our custom claims we can retrive them calling `claim<T>(name: String) -> T?` where `T` is the value type of the claim, e.g.: a `String`
 
-    _ = jwt.claim(name: "email").string
+    // Custom Claims
+    // You can retrieve a custom claim through a subscript and then attempt to convert the value to a specific type.
+    _ = jwt["email"].string
+    _ = jwt["custom"].rawValue as? [Int]
 
-    _ = jwt.claim(name: "custom").rawValue as? [Int]
-//: ### Error Handling
-//: If the token is invalid an `DecodeError` will be thrown
+    // Error Handling
+    // If the token is invalid an `DecodeError` will be thrown.
 } catch let error as DecodeError {
     error.localizedDescription
 }
