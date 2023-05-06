1b7de1adc5e65bde0292733fbec478a9cfd1845d
Rita Zerrizuela <zeta@widcket.com>
2023-05-05 23:34:38 -0300

    Update Quick and Nimble to latest major

--- Package.swift
+++ Package.swift
@@ -7,8 +7,8 @@ let package = Package(
     platforms: [.iOS(.v12), .macOS(.v10_15), .tvOS(.v12), .watchOS("6.2")],
     products: [.library(name: "JWTDecode", targets: ["JWTDecode"])],
     dependencies: [
-        .package(name: "Quick", url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "5.0.0")),
-        .package(name: "Nimble", url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "10.0.0"))
+        .package(name: "Quick", url: "https://github.com/Quick/Quick.git", .upToNextMajor(from: "6.0.0")),
+        .package(name: "Nimble", url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "12.0.0"))
     ],
     targets: [
         .target(
